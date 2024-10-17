3667b41a5a0104cc2fb9da3daca1338673301560
ORiVS <107984700+ORiVS@users.noreply.github.com>
2024-10-17 11:10:48 +0200

    feat: add authentication system

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,9 +1,20 @@
 <?php
 
 use Livewire\Volt\Component;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
-new class extends Component {
-    //
+new class extends Component
+{
+    public function logout(): void
+    {
+        Auth::guard('web')->logout();
+
+        Session::invalidate();
+        Session::regenerateToken();
+
+        $this->redirect('/');
+    }
 }; ?>
 
 <div>
@@ -12,14 +23,14 @@ new class extends Component {
         {{-- User --}}
         @if($user = auth()->user())
             <x-menu-separator />
-
-            <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="-mx-2 !-my-2 rounded">
-                <x-slot:actions>
-                    <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate link="/logout" />
-                </x-slot:actions>
-            </x-list-item>
-
+                <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="-mx-2 !-my-2 rounded">
+                    <x-slot:actions>
+                        <x-button icon="o-power" wire:click="logout" class="btn-circle btn-ghost btn-xs" tooltip-left="{{__('Logout')}}" no-wire-navigate />
+                    </x-slot:actions>
+                </x-list-item>
             <x-menu-separator />
+        @else
+            <x-menu-item title="{{__('Login')}}" icon="o-user" link="{{ route('login') }}" />
         @endif
 
         <x-menu-item title="Hello" icon="o-sparkles" link="/" />
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
 use Livewire\Volt\Volt;
+use Illuminate\Support\Facades\Route;
+
 
 Volt::route('/', 'index');
+
+Route::middleware('guest')->group(function () {
+    Volt::route('/register', 'auth.register');
+    Volt::route('/login', 'auth.login')->name('login');
+    Volt::route('/forgot-password', 'auth.forgot-password');
+    Volt::route('/reset-password/{token}', 'auth.reset-password')->name('password.reset');
+});
